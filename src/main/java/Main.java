54f8b626d2dd8cc0752ff08cc9fa10ea9fa7636c
Kwon Young Jae <kyoje11@gmail.com>
2018-11-02 14:04:41 +0900

    Update Input & Out View & Write Scenario

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import view.InputView;
+
+public class Main {
+	public static void main(String[] args) {
+		InputView.printInputView();
+		// Input Price of Lotto
+		// Print count of Lotto
+		// Print Lotto Number List
+		// Print Prize Lotto Number List
+		// Print Statistic Prize
+	}
+}
+
